c91eef51e4be9ce0329fb3d9b1eb5e3553a3b910
Lukas Wallmann <lukas.wallmann@yahoo.de>
2017-07-26 19:44:10 +0200

    linebreak for test
    Signed-off-by: Lukas Wallmann <lukas.wallmann@yahoo.de>

--- config.php
+++ config.php
@@ -4,4 +4,5 @@ $basedir=dirname(__FILE__);
 $baseurlws=""; //if installed in a subfolder, enter subfolder without slash in front, if run in root set to ""
 $baseurl="".$baseurlws; //if in a subfolder change to "/".baseurlws
 
+
 ?>
