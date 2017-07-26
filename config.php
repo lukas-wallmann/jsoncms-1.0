0c21d8a165ad575b56eb12cc0339de22d900201a
Lukas Wallmann <lukas.wallmann@yahoo.de>
2017-07-26 20:04:26 +0200

    jquery update

--- config.php
+++ config.php
@@ -4,5 +4,4 @@ $basedir=dirname(__FILE__);
 $baseurlws=""; //if installed in a subfolder, enter subfolder without slash in front, if run in root set to ""
 $baseurl="".$baseurlws; //if in a subfolder change to "/".baseurlws
 
-
 ?>
