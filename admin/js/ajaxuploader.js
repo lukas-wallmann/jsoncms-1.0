fdaac803c37b72cc74adc321aaf7ebf276125e35
Lukas Wallmann <lukas.wallmann@yahoo.de>
2017-08-24 18:56:21 +0200

    working on new uploader

--- admin/js/ajaxuploader.js
+++ admin/js/ajaxuploader.js
@@ -23,8 +23,62 @@ $.fn.ajaxUploader = function(multiple,done,error,dir) {
       $(".ajaxUploader_"+instance.uploaderID+" input[type=file]").on('change', prepareUpload);
       $(".ajaxUploader_"+instance.uploaderID).on('submit', uploadFiles);
     }
-    function prepareUpload (event){
-        instance.files = event.target.files;
+    function prepareUpload (evt){
+      var files = evt.target.files; // FileList object
+
+      // Loop through the FileList and render image files as thumbnails.
+      for (var i = 0, f; f = files[i]; i++) {
+
+        var reader = new FileReader();
+
+        reader.onload = (function(theFile) {
+          return function(e) {
+            // Render thumbnail.
+            if (theFile.type=="image/jpeg") {
+              resize(e.target.result,[200,200],"thump_"+theFile.name,callback);
+              resize(e.target.result,[1920,1080],theFile.name,callback);
+            }else{
+              callback(e.target.result,theFile.name)
+            }
+          };
+        })(f);
+
+        reader.readAsDataURL(f);
+      }
+    }
+    function callback(data,filename){
+      instance.files.push([filename,data]);
+    }
+    function resize(url,info=[1920,1080],name="file.jpg",callback=function(){},q=0.8){
+      var image = new Image();
+      image.onload = function (imageEvent) {
+
+          // Resize the image
+          var canvas = document.createElement('canvas'),
+              width = image.width,
+              height = image.height,
+              widthto=image.info[0],
+              heigthto=image.info[1],
+              relw = widthto/width,
+              relh = heigthto/height;
+
+          if (relh > relw) {
+              relto=relw;
+          } else {
+              relto=relh;
+          }
+          width*=relto;
+          height*=relto;
+          canvas.width = width;
+          canvas.height = height;
+          canvas.getContext('2d').drawImage(image, 0, 0, width, height);
+          var dataUrl = canvas.toDataURL('image/jpeg',q);
+          callback(dataUrl,image.name);
+      }
+
+      image.src = url
+      image.info=info;
+      image.name=name;
     }
     function uploadFiles(event){
           event.stopPropagation();
